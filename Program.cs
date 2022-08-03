aba6ce66bf8266b6c311c7edec6c334ed8aba2d5
ridhwan2 <110493782+ridhwan2@users.noreply.github.com>
2022-08-03 19:00:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.Title = "C#";
+Console.ForegroundColor = ConsoleColor.Blue;
+
+int things;
+
+Console.WriteLine("How many things do you want to sort?");
+
+string[] thing = new string [things = Convert.ToInt32(Console.ReadLine())];
+
+Console.WriteLine("Enter some things to sort alphabetically.");
+
+for (int i = 0; i < thing.Length; i++)
+{
+    thing[i] = Console.ReadLine();
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine();
+Console.WriteLine("Done.");
+
+Array.Sort(thing);
+
+for (int i = 0; i < thing.Length; i++)
+{
+    Console.WriteLine(thing[i]);
+}
+Console.ReadKey();
